fbbde4da48ef19446855a1293e6e75e6f20cb471
konnack <kozo.noemi@gmail.com>
2020-04-17 10:29:25 +0200

    Third part

--- scripts.js
+++ scripts.js
@@ -2,6 +2,30 @@ let name = "Adam"
 
 console.log (`Hello, my dear ${name}`); 
 
+//jQuery part
+$ ('p').html ('The <strong>jQuery</strong>  part has started!')
+
+$ ('h1').css ('background', 'red')
+
+$ ('h2').toggleClass ('green');
+$ ('p').toggleClass ('green');
+$ ('p').toggleClass ('green');
+
+$('main').append ('<p id="first">Added element</p>')
+$('main').append ('<p id="second">Added second element</p>')
+
+$('#first').remove();
+
+$('#add-button').click(() => {
+    $('main').append ('<p> Added a new p tag </p>');
+});
+
+let removeFunction = () => {
+    $('main p:last-child').remove ();
+}
+
+$('#remove-button').click(removeFunction);
+
 //First Part from the Developer Console
 
 //123
